955eb8226d3ea4994c77fdfeebbd441f7c808ef6
Richard Nader Jr <richard.nader@fynydd.com>
2018-09-19 20:13:06 -0400

    Added security and error handling.

---
+++ SpringBootSandbox/src/main/java/com/potatosaucevfx/springbootsandbox/model/Person.java
@@ -0,0 +1,11 @@
+package com.potatosaucevfx.springbootsandbox.model;
+
+/**
+ * 
+ * @author Richard Nader Jr. <heelyskidrj@gmail.com>
+ */
+public class Person {
+    
+    private String firstName;
+    private String lastName;
+}
